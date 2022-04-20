0aa2096f7a09213976f1fcc1da311b38e400fec4
jonas <jonas.gysin@students.fhnw.ch>
2022-04-20 19:35:07 +0200

    add model for Bean add model for CoffeeMix

--- src/main/java/ch/fhnw/webec/exercise/model/Bean.java
+++ src/main/java/ch/fhnw/webec/exercise/model/Bean.java
@@ -1,4 +1,55 @@
 package ch.fhnw.webec.exercise.model;
 
+import javax.persistence.*;
+
+@Entity
 public class Bean {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String type;
+
+    @Column
+    private String origin;
+
+    @Column
+    private int altitude;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private CoffeeMix coffeeMix;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public int getAltitude() {
+        return altitude;
+    }
+
+    public void setAltitude(int altitude) {
+        this.altitude = altitude;
+    }
 }
