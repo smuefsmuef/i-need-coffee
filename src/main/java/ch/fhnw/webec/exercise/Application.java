6a32f92c1b25ace756957b5a2e26154df613d85c
jonas <jonas.gysin@students.fhnw.ch>
2022-04-20 18:51:03 +0200

    setup project

--- src/main/java/ch/fhnw/webec/exercise/Application.java
+++ src/main/java/ch/fhnw/webec/exercise/Application.java
@@ -1,9 +1,12 @@
 package ch.fhnw.webec.exercise;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.data.jpa.repository.config.*;
+
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
